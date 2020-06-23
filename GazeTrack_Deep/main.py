55453d22bd0e49a1d26fb0e3b807edb19605117e
Avish <avish2806@gmail.com>
2020-06-23 18:30:27 +0530

    Update main.py

--- GazeTrack_Deep/main.py
+++ GazeTrack_Deep/main.py
@@ -1,15 +1,16 @@
-mport argparse
+import argparse
 import os
 import queue
 import threading
 import time
-from datasources import Video, Webcam
 import coloredlogs
 import cv2 as cv
 import numpy as np
 import tensorflow as tf
+
+from datasources import Video, Webcam
+from models import nnarch
 import util.gaze
-import nnarch
 
 if __name__ == '__main__':
 
@@ -31,11 +32,12 @@ if __name__ == '__main__':
         fmt='%(asctime)s %(levelname)s %(message)s',
         level=args.v.upper(),
     )
-
     tf.logging.set_verbosity(tf.logging.INFO)
     with tf.Session(config=session_config) as session:
 
+        
         batch_size = 2
+
         if args.from_video:
             assert os.path.isfile(args.from_video)
             data_source = Video(args.from_video,
@@ -60,7 +62,7 @@ if __name__ == '__main__':
                     },
                 ],
             )
-         else:
+        else:
             model = nnarch(
                 session, train_data={'videostream': data_source},
                 first_layer_stride=1,
@@ -72,48 +74,48 @@ if __name__ == '__main__':
                     },
                 ],
             )
-            
-          if args.record_video:
-              video_out = None
-              video_out_queue = queue.Queue()
-              video_out_should_stop = False
-              video_out_done = threading.Condition()
 
-              def _record_frame():
-                  global video_out
-                  last_frame_time = None
-                  out_fps = 30
-                  out_frame_interval = 1.0 / out_fps
-                  while not video_out_should_stop:
-                      frame_index = video_out_queue.get()
-                      if frame_index is None:
-                          break
-                      assert frame_index in data_source._frames
-                      frame = data_source._frames[frame_index]['bgr']
-                      h, w, _ = frame.shape
-                      if video_out is None:
-                          video_out = cv.VideoWriter(
-                              args.record_video, cv.VideoWriter_fourcc(*'H264'),
-                              out_fps, (w, h),
-                          )
-                      now_time = time.time()
-                      if last_frame_time is not None:
-                          time_diff = now_time - last_frame_time
-                          while time_diff > 0.0:
-                              video_out.write(frame)
-                              time_diff -= out_frame_interval
-                      last_frame_time = now_time
-                  video_out.release()
-                  with video_out_done:
-                      video_out_done.notify_all()
-              record_thread = threading.Thread(target=_record_frame, name='record')
-              record_thread.daemon = True
-              record_thread.start()
-              
+        
+        if args.record_video:
+            video_out = None
+            video_out_queue = queue.Queue()
+            video_out_should_stop = False
+            video_out_done = threading.Condition()
+
+            def _record_frame():
+                global video_out
+                last_frame_time = None
+                out_fps = 30
+                out_frame_interval = 1.0 / out_fps
+                while not video_out_should_stop:
+                    frame_index = video_out_queue.get()
+                    if frame_index is None:
+                        break
+                    assert frame_index in data_source._frames
+                    frame = data_source._frames[frame_index]['bgr']
+                    h, w, _ = frame.shape
+                    if video_out is None:
+                        video_out = cv.VideoWriter(
+                            args.record_video, cv.VideoWriter_fourcc(*'H264'),
+                            out_fps, (w, h),
+                        )
+                    now_time = time.time()
+                    if last_frame_time is not None:
+                        time_diff = now_time - last_frame_time
+                        while time_diff > 0.0:
+                            video_out.write(frame)
+                            time_diff -= out_frame_interval
+                    last_frame_time = now_time
+                video_out.release()
+                with video_out_done:
+                    video_out_done.notify_all()
+            record_thread = threading.Thread(target=_record_frame, name='record')
+            record_thread.daemon = True
+            record_thread.start()
+            
          if args.record_video and video_out is not None:
             video_out_should_stop = True
             video_out_queue.put_nowait(None)
             with video_out_done:
                 video_out_done.wait()
-            
                 
